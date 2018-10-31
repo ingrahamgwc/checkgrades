0816797f747c4b8092adcb1ee436b6e9f354d26c
Ingraham Girls Who Code <32528536+ingrahamgwc@users.noreply.github.com>
2018-10-31 15:35:47 -0700

    Create check.user.js

---
+++ check.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name          checkgrades
+// @namespace     http://github.com/ingrahamgwc
+// @description   check your grades
+// @include       https://ps.seattleschools.org/*
+// ==/UserScript==
+
+alert("hi");
