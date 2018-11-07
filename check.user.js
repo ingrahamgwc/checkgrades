636f8bded9b460cdd0963bf28830bb79d4e8e1e8
kirstenmg <32720576+kirstenmg@users.noreply.github.com>
2018-11-07 15:08:51 -0800

    Update check.user.js

--- check.user.js
+++ check.user.js
@@ -2,7 +2,7 @@
 // @name          checkgrades
 // @namespace     http://github.com/ingrahamgwc
 // @description   check your grades
-// @include       https://ps.seattleschools.org/*
+// @include       https://ps.seattleschools.org/guardian/home.html
 // ==/UserScript==
 
 alert("hi");
