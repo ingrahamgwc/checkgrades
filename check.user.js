09f41900e1e9a918a4e71e326ff04f882dd94173
mayaouthred <mayaouthred@gmail.com>
2018-11-16 09:47:33 -0800

    gets course names

--- check.user.js
+++ check.user.js
@@ -5,4 +5,33 @@
 // @include       https://ps.seattleschools.org/guardian/home.html
 // ==/UserScript==
 
-alert("hi");
+alert("hello");
+
+
+var elementlist = document.getElementsByTagName("td");
+
+
+function getCol(tableid, col) {
+  var table = document.getElementById(tableid);
+  var n = table.rows.length;
+  var s = "";
+  
+  for (var i = 3; i < n; i++){
+    var tr = table.rows[i];
+    if (tr.cells.length > col){
+      var td = tr.cells[col];
+      
+      var str = td.textContent;
+      var index = str.indexOf("Details");
+      str = str.slice(0, index);
+      s += " " + str;
+    }
+  }
+  
+  return s;
+  
+}
+
+var names = getCol("tblgrades", 11);
+alert(names);
+
